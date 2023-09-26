2b193f838a617b8457284f44e712cffacd45e523
Naz <mercy.kamau@student.moringaschool.com>
2023-09-26 17:46:39 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+function WelcomeMessage (GraceHopper) {
+    return 'Welcome to Flatbook,${GraceHopper}!' ;
+}
+function excitedWelcomeMessage(GraceHopper) {
+    return `Welcome to Flatbook, ${GraceHopper.toUpperCase()}!`;
+  }
+function shortGreeting(GraceHopper)! {
+    return `Welcome, ${GraceHopper.charAt(0)}!`;
+  }
+module.exports = {
+    currentUser,
+    welcomeMessage,
+    excitedWelcomeMessage,
+    shortGreeting,
+  };
+    // Write your code in this file!
